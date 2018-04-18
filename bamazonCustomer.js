6c271d94f2306e5b28ed23df8f9e64ae20a8c0ef
MichaelWrobelPersonal <michaelrwrobel@sbcglobal.net>
2018-04-17 23:24:17 -0400

    Basic Customer Functionality implemented

---
+++ bamazonCustomer.js
@@ -0,0 +1,103 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  customerService();
+});
+
+function customerService() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Take a look at the shop",
+        "Order something"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Take a look at the shop":
+        showProducts();
+        break;
+
+      case "Order something":
+        placeOrder();
+        break;
+      }
+    });
+}
+
+function showProducts() {
+    var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM bamazon.products";
+    connection.query(query, "*", function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+        console.log("id: " + res[i].item_id + " || Product: " + res[i].product_name + " || Department: " + res[i].department_name + "|| Price:  " + res[i].price + "|| Quantity: " + res[i].stock_quantity );
+        }
+    });
+}
+
+function placeOrder() {
+    inquirer
+      .prompt([{
+        name: "item_id",
+        type: "input",
+        message: "What item would you like to purchase?"
+      },
+      {
+        name: "item_qty",
+        type: "input",
+        message: "How many would you like to purchase?"
+      }
+    ])
+      .then(function(answer) {
+        var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM bamazon.products WHERE item_id=?";
+        connection.query(query, { item_id: answer.item_id }, function(err, res) {
+          for (var i = 0; i < res.length; i++) {
+            console.log("id: " + res[i].item_id + " || Product: " + res[i].product_name + " || Department: " + res[i].department_name + "|| Price:  " + res[i].price + "|| Quantity: " + res[i].stock_quantity );
+          }
+          if (res[0].stock_quantity > answer.item_qty)
+             updateProductStock(res[0].item_id, res[0].stock_quantity-answer.item_qty);
+          else
+             console.log("Insufficient quantity in stock, try again.");
+        });
+      });
+}  
+
+function updateProductStock(choosen_item_id, updated_qty) {
+    console.log("Updating Stock Quantity...\n");
+    var query = connection.query(
+      "UPDATE bamazon.products SET ? WHERE ?",
+      [
+        {
+          stock_quantity: updated_qty
+        },
+        {
+          item_id: choosen_item_id
+        }
+      ],
+      function (err, res) {
+        if (err) {
+            throw err;
+        }
+        console.log(res.affectedRows + " stock quantity updated!\n");
+      }
+    );
+    console.log(query.sql);
+}
+
+
